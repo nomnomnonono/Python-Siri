3605ed43d9f7689781b182d2f0db3c4bfd09c749
nomnomnonono <murumuru1204@gmail.com>
2023-04-03 12:09:30 +0900

    add prompt setting

--- app.py
+++ app.py
@@ -14,33 +14,86 @@ def transcribe(filepath):
     return result.text
 
 
-def answer_by_chat(question, api_key):
+def answer_by_chat(
+    question,
+    role1,
+    content1,
+    role2,
+    content2,
+    role3,
+    content3,
+    role4,
+    content4,
+    role5,
+    content5,
+    api_key,
+):
     openai.api_key = api_key
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "あなたは役に立つアシスタントです。"},
-            {"role": "user", "content": question},
-        ],
-    )
+    messages = [
+        {"role": role, "content": content}
+        for role, content in [
+            [role1, content1],
+            [role2, content2],
+            [role3, content3],
+            [role4, content4],
+            [role5, content5],
+        ]
+        if role != "" and content != ""
+    ]
+    messages.append({"role": "user", "content": question})
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
     return response["choices"][0]["message"]["content"]
 
 
 with gr.Blocks() as demo:
-    gr.Markdown("Search fairness paper using this demo.")
-    api_key = gr.Textbox(label="Paste your own openai-api-key")
-    with gr.Row():
-        audio_input = gr.Audio(
-            source="microphone", type="filepath", label="Record from microphone"
-        )
-        audio_button = gr.Button("Transcribe")
-    audio_output = gr.Textbox()
+    gr.Markdown("Siri-like application via Whisper and ChatGPT")
+    with gr.Tabs():
+        with gr.TabItem(label="General"):
+            api_key = gr.Textbox(label="Paste your own openai-api-key")
+            with gr.Row():
+                audio_input = gr.Audio(
+                    source="microphone", type="filepath", label="Record from microphone"
+                )
+                audio_button = gr.Button("Transcribe")
+            audio_output = gr.Textbox()
+            chat_button = gr.Button("Questions to ChatGPT")
+            chat_output = gr.Textbox()
+        with gr.TabItem(label="Setting"):
+            gr.Markdown("Prompt Setting")
+            with gr.Row():
+                role1 = gr.Dropdown(["system", "user", "assistant"], value="system")
+                content1 = gr.Textbox(value="あなたは役に立つアシスタントです。")
+            with gr.Row():
+                role2 = gr.Dropdown(["system", "user", "assistant"])
+                content2 = gr.Textbox()
+            with gr.Row():
+                role3 = gr.Dropdown(["system", "user", "assistant"])
+                content3 = gr.Textbox()
+            with gr.Row():
+                role4 = gr.Dropdown(["system", "user", "assistant"])
+                content4 = gr.Textbox()
+            with gr.Row():
+                role5 = gr.Dropdown(["system", "user", "assistant"])
+                content5 = gr.Textbox()
     audio_button.click(
         transcribe, inputs=[audio_input], outputs=[audio_output], api_name="transcribe"
     )
-    chat_button = gr.Button("Questions to ChatGPT")
-    chat_output = gr.Textbox()
     chat_button.click(
-        answer_by_chat, inputs=[audio_output, api_key], outputs=[chat_output]
+        answer_by_chat,
+        inputs=[
+            audio_output,
+            role1,
+            content1,
+            role2,
+            content2,
+            role3,
+            content3,
+            role4,
+            content4,
+            role5,
+            content5,
+            api_key,
+        ],
+        outputs=[chat_output],
     )
 demo.launch()
